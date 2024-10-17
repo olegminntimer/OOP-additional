b46e3b67dd937ef4ea74b0b8fcadd27ec6b26f6b
Oleg Timer... <olegtimer@yandex.ru>
2024-10-17 13:52:42 +0500

    lesson 14.2

--- src/product.py
+++ src/product.py
@@ -10,5 +10,45 @@ class Product:
         """Инициализация экземпляра класса Product"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, name, description,  price, quantity):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, amount):
+        if amount <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return None
+        self.__price = amount
+        return self.__price
+
+
+
+# if __name__ == "__main__":
+#     product1 = Product(
+#         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+#     )
+#     # print(product1.name)
+#     # print(product1.description)
+#     # print(product1.price)
+#     # print(product1.quantity)
+#     #
+#     product2 = Product.new_product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#     #
+#     # print(product2.name)
+#     # print(product2.description)
+#     # print(product2.price)
+#     # print(product2.quantity)
+#     print(product1.price)
+#     product2.price = 40000
+#     print(product2.price)
+#     product2.price = -40000
+#     print(product2.price)
+
--- tests/test_category.py
+++ tests/test_category.py
@@ -14,4 +14,18 @@ def test_category_init(first_category, second_category):
         first_category.description
         == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
-    assert len(first_category.products) == 3
+    assert len(first_category.products_in_list) == 3
+
+
+def test_category_products_property(first_category):
+    """Тест проверяет геттер свойства products"""
+    assert first_category.products ==("Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                      "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+                                      "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n")
+
+
+def test_category_products_setter(first_category, product):
+    """Тест проверяет сеттер свойства products"""
+    assert len(first_category.products_in_list) == 3
+    first_category.add_product = product
+    assert len(first_category.products_in_list) == 4
