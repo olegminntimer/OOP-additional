5bc2edca95ac0b5ba3c29eb1ad2f00b454e66fa6
Oleg Timer... <olegtimer@yandex.ru>
2024-10-19 23:50:35 +0500

    finish

--- src/category.py
+++ src/category.py
@@ -19,7 +19,7 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
-    def __str__(self):
+    def __str__(self) -> str:
         products_count = 0
         for product_ in self.__products:
             products_count += product_.quantity
@@ -40,27 +40,29 @@ class Category:
     def products_in_list(self) -> list:
         return self.__products
 
-    def sum_of_cost_all_goods(self):
-        """Функция считает стоимость всех товаров в категории"""
-        product_sum = Product("Стоимость всего", "Стоимость всех товаров в категории", 0, 0)
-        for prod in self.__products:
-            product_sum.sum_price = product_sum + prod
-            # product_sum.quantity += prod.quantity
-        return product_sum.sum_price
 
-if __name__ == "__main__":
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+def sum_of_cost_all_goods(products_: list) -> float:
+    """Функция считает стоимость всех товаров в категории"""
+    product_sum = Product("Стоимость всего", "Стоимость всех товаров в категории", 0, 0)
+    for prod in products_:
+        product_sum.sum_price = product_sum + prod
+        # product_sum.quantity += prod.quantity
+    return product_sum.sum_price
 
-    category1 = Category(
-        "Смартфоны",
-        """Смартфоны, как средство не только коммуникации, но и получения дополнительных
-функций для удобства жизни""",
-        [product1, product2, product3],
-    )
+
+# if __name__ == "__main__":
+#     product1 = Product(
+#         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+#     )
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         """Смартфоны, как средство не только коммуникации, но и получения дополнительных
+# функций для удобства жизни""",
+#         [product1, product2, product3]
+#     )
 #     print(category1.name)
 #     print(category1.description)
 #     print(category1.products)
@@ -68,9 +70,8 @@ if __name__ == "__main__":
 #
 #     product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 #     category1.add_product(product4)
-    print(category1.products)
-    print(Category.product_count)
-    print(category1)
-
-    print(category1.sum_of_cost_all_goods())
-
+#     print(category1.products)
+#     print(Category.product_count)
+#     print(category1)
+#
+# print(category1.sum_of_cost_all_goods(category1.products_in_list))
--- src/product_iterator.py
+++ src/product_iterator.py
@@ -1,18 +1,23 @@
-from src.product import Product
+# from src.product import Product
+# from src.category import Category
+from collections.abc import Iterator, Callable
+from typing import Any
+
 from src.category import Category
 
 
 class ProductIterator:
     """Вспомогательный класс, с помощью которого можно перебирать товары одной категории, например в цикле for"""
-    def __init__(self, category_obj):
+
+    def __init__(self, category_obj: Category) -> None:
         self.category = category_obj
         self.index = 0
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator:
         self.index = 0
         return self
 
-    def __next__(self):
+    def __next__(self) -> Any:
         if self.index < len(self.category.products_in_list):
             product = self.category.products_in_list[self.index]
             self.index += 1
@@ -20,27 +25,26 @@ class ProductIterator:
         else:
             raise StopIteration
 
-if __name__ == "__main__":
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    category1 = Category(
-        "Смартфоны",
-        """Смартфоны, как средство не только коммуникации, но и получения дополнительных
-функций для удобства жизни""",
-        [product1, product2, product3],
-    )
-
-    iterator = ProductIterator(category1)
-
-    for pr in iterator:
-        print(pr)
-
-    print()
-    for pr in iterator:
-        print(pr)
-
-
+# if __name__ == "__main__":
+#     product1 = Product(
+#         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+#     )
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     category1 = Category(
+#         "Смартфоны",
+#         """Смартфоны, как средство не только коммуникации, но и получения дополнительных
+# функций для удобства жизни""",
+#         [product1, product2, product3],
+#     )
+#
+#     iterator = ProductIterator(category1)
+#
+#     for pr in iterator:
+#         print(pr)
+#
+#     print()
+#     for pr in iterator:
+#         print(pr)
