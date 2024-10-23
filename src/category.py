901f87d025828077fa64a6776b8091870c977727
Oleg Timer... <olegtimer@yandex.ru>
2024-10-23 17:58:13 +0500

    add main

--- src/category.py
+++ src/category.py
@@ -49,28 +49,28 @@ def sum_of_cost_all_goods(products_1: list) -> float:
     return product_sum
 
 
-if __name__ == "__main__":
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "Серый"
-    )
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8, "Gray space")
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, "Синий")
-
-    category1 = Category(
-        "Смартфоны",
-        """Смартфоны, как средство не только коммуникации, но и получения дополнительных
-функций для удобства жизни""",
-        [product1, product2, product3]
-    )
-    print(category1.name)
-    print(category1.description)
-    print(category1.products)
-    print(Category.product_count)
-
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7, "")
-    category1.add_product(product4)
-    print(category1.products)
-    print(Category.product_count)
-    print(category1)
-
-    print(sum_of_cost_all_goods([product1, product2, product3]))
+# if __name__ == "__main__":
+#     product1 = Product(
+#         "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "Серый"
+#     )
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8, "Gray space")
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, "Синий")
+# 
+#     category1 = Category(
+#         "Смартфоны",
+#         """Смартфоны, как средство не только коммуникации, но и получения дополнительных
+# функций для удобства жизни""",
+#         [product1, product2, product3]
+#     )
+#     print(category1.name)
+#     print(category1.description)
+#     print(category1.products)
+#     print(Category.product_count)
+# 
+#     product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7, "")
+#     category1.add_product(product4)
+#     print(category1.products)
+#     print(Category.product_count)
+#     print(category1)
+# 
+#     print(sum_of_cost_all_goods([product1, product2, product3]))
