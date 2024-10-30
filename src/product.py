a7a8bb55062c879a189143636f7b1c42930ef5d5
Oleg Timer... <olegtimer@yandex.ru>
2024-10-30 14:34:25 +0500

    исключение добавления товара с нулевым количествомн

--- src/product.py
+++ src/product.py
@@ -5,13 +5,16 @@ from src.print_mixin import PrintMixin
 class Product(BaseProduct, PrintMixin):
     """Класс описывает продукт"""
 
-    def __init__(self, name: str, description: str, price: float, quantity: int, color: str):
+    def __init__(self, name: str, description: str, price: float, quantity: int, color: str=""):
         """Инициализация экземпляра класса Product"""
         self.name = name
         self.description = description
         self.color = color
         self.__price = price
-        self.quantity = quantity
+        if quantity != 0:
+            self.quantity = quantity
+        else:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         super().__init__()
 
     def __str__(self) -> str:
@@ -38,13 +41,13 @@ class Product(BaseProduct, PrintMixin):
         self.__price = amount
 
 
-# if __name__ == "__main__":
-#     product0 = Product("", "", 0, 0, 0)
-#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "Серый")
-#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8, "Gray space")
-#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, "Синий")
-#     for i in (product1, product3):
-#         product0.sum_price = product0 + i
-#
-#     print(product0.sum_price)
-#     print(product1)
+if __name__ == "__main__":
+    product0 = Product("", "", 0, 0, 0)
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "Серый")
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8, "Gray space")
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, "Синий")
+    # for i in (product1, product3):
+    #     product0.sum_price = product0 + i
+    #
+    # print(product0.sum_price)
+    # print(product1)
